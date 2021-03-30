93a1e47dea5eb527021f751c113680176b807a57
codicem69 <codicem69@gmail.com>
2021-03-30 10:15:25 +0200

    modifica prodotto

--- packages/salt/model/movim_righe.py
+++ packages/salt/model/movim_righe.py
@@ -3,7 +3,7 @@
 
 class Table(object):
     def config_db(self,pkg):
-        tbl =  pkg.table('movim_righe',pkey='id',name_long='movim_righe',name_plural='movim_righe',caption_field='prodotti_id')
+        tbl =  pkg.table('movim_righe',pkey='id',name_long='movim_righe',name_plural='movim_righe',caption_field='prodotto_id')
         self.sysFields(tbl)
         tbl.column('movim_id',size='22',name_long='movimentazione').relation('movim.id',relation_name='righemov', mode='foreignkey', onDelete='cascade')
         tbl.column('prodotto_id',size='22',name_long='prodotto').relation('prodotto.id',relation_name='prodmovrig', mode='foreignkey', onDelete='raise')
--- packages/salt/model/prodotto.py
+++ packages/salt/model/prodotto.py
@@ -7,3 +7,14 @@ class Table(object):
         self.sysFields(tbl)
         tbl.column('cod',name_long='codice')
         tbl.column('descrizione',name_long='descrizione')
+        tbl.formulaColumn('tot_carico_prod',select=dict(table='salt.movim_righe',
+                                                  columns='SUM($quantita)',
+                                                  where='$prodotto_id=#THIS.id'),
+                                               dtype='N',name_long='Tot.Carico Prod.')
+        
+        tbl.formulaColumn('tot_scarico_prod',select=dict(table='salt.movim_righe',
+                                                  columns='SUM($quantita)',
+                                                  where='$prodotto_id=#THIS.id'),
+                                               dtype='N',name_long='Tot.Scarico Prod.')
+        
+        tbl.formulaColumn('rimanenza_prod',"coalesce($tot_carico_prod,0) - coalesce($tot_scarico_prod,0)")
