6636392067d236604a18e016f42678b9dca3dfca
tommaso <codicem69@gmail.com>
2022-10-06 18:08:26 +0200

    modificato il menu

--- packages/salt/menu.py
+++ packages/salt/menu.py
@@ -1,10 +1,22 @@
 # encoding: utf-8
 class Menu(object):
     def config(self,root,**kwargs):
-        salt = root.branch(u"salt", tags="")
-        salt.thpage(u"movimentazione", table="salt.movim", tags="")
-        salt.thpage(u"movim_prodotti", table="salt.movim_righe", tags="", viewResource="ViewMovimentiProd")
-        salt.thpage(u"movim_righe", table="salt.movim_righe", tags="")
-        salt.thpage(u"prodotti", table="salt.prodotto", tags="")
-        salt.lookups(u"Lookup tables", lookup_manager="salt")
+        user=self.db.currentEnv.get('user')
+
+        if user != 'admin':
+            salt = root.branch(u"salt", tags="")
+            salt.thpage(u"movimentazione", table="salt.movim", tags="")
+            salt.thpage(u"movim_prodotti", table="salt.movim_righe", tags="", viewResource="ViewMovimentiProd")
+            salt.thpage(u"movim_righe", table="salt.movim_righe", tags="")
+            salt.thpage(u"prodotti", table="salt.prodotto", tags="")
+            salt.lookups(u"Lookup tables", lookup_manager="salt")
+        else:
+            salt = root.branch(u"salt", tags="")
+            salt.packageBranch('Amministrazione sistema',pkg='adm')
+            salt.packageBranch('System',pkg='sys')
+            salt.thpage(u"movimentazione", table="salt.movim", tags="")
+            salt.thpage(u"movim_prodotti", table="salt.movim_righe", tags="", viewResource="ViewMovimentiProd")
+            salt.thpage(u"movim_righe", table="salt.movim_righe", tags="")
+            salt.thpage(u"prodotti", table="salt.prodotto", tags="")
+            salt.lookups(u"Lookup tables", lookup_manager="salt")
 
