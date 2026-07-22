07bb7e340fd15a245f4969d96a902a6ff7fd5e53
codicem69 <codicem69@gmail.com>
2026-07-22 15:06:21 +0200

    modificata schermata iniziale

--- packages/salt/webpages/index.py
+++ packages/salt/webpages/index.py
@@ -3,3 +3,37 @@
 class GnrCustomWebPage(object):
     py_requires = 'plainindex'
     
+    @property
+    def index_url(self):
+        return None
+
+    def index_dashboard(self, root):
+
+
+        wrapper = root.div(width='100%', height='100%',
+                           text_align='center', background='white',
+                           padding_top='30px')
+        wrapper.img(src='/_pkg/salt/resources/html_pages/images/Ranalli_Logo.svg',
+                    style='width:20%;margin-top:30px;')
+
+        titlebox = wrapper.div(display='flex',
+                               justify_content='center',
+                               align_items='center',
+                               gap='20px',
+                               margin_top='20px',
+                               margin_bottom='30px')
+
+        titlebox.img(src='/_pkg/salt/resources/html_pages/images/salt.svg',
+                     style='width:100px;height:100px;')
+
+        titlebox.div(
+                'Movimentazione Sale<br>ITALKALI</br>',
+                font_size='60px',
+                font_weight='bold',
+                color='#384D63',
+                text_shadow='2px 2px 4px rgba(0,0,0,0.15)')
+
+
+        #titlebox.div('Shipsteps', font_size='100px', color='#384D63')
+
+
